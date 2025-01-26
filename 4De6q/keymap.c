f4a5ef25ac6f4a9433da46d00e46f8ad46be3427
Thomas Anderson <thomas@heythomasj.com>
2025-01-25 16:33:16 -0800

    fix: oryx conflict when removing the double tap

--- 4De6q/keymap.c
+++ 4De6q/keymap.c
@@ -3,6 +3,8 @@
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
+#include "features/achordion.h"
+
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
@@ -105,6 +107,7 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
 
     case RGB_SLD:
@@ -120,4 +123,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
+void matrix_scan_user(void) {
+  achordion_task();
+}
+
+uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+ switch (tap_hold_keycode) {
+    default:
+      return 800;  // Otherwise use a timeout of 800 ms.
+ }
+}
+
+bool achordion_eager_mod(uint8_t mod) {
+  switch (mod) {
+    case MOD_LSFT:
+    case MOD_RSFT:
+    case MOD_LCTL:
+    case MOD_RCTL:
+      return true;  // Eagerly apply Shift and Ctrl mods.
+
+    default:
+      return false;
+  }
+}
+
+//uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {
+//  // If you quickly hold a tap-hold key after tapping it, the tap action is
+//  // repeated. Key repeating is useful e.g. for Vim navigation keys, but can
+//  // lead to missed triggers in fast typing. Here, returning 0 means we
+//  // instead want to "force hold" and disable key repeating.
+//  switch (keycode) {
+//    case HOME_N:
+//    // Repeating is useful for Vim navigation keys.
+//    case QHOME_J:
+//    case QHOME_K:
+//    case QHOME_L:
+//      return QUICK_TAP_TERM;  // Enable key repeating.
+//    default:
+//      return 0;  // Otherwise, force hold and disable key repeating.
+//  }
+//}
 
